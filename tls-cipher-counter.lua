50290527237ec896d96c79adc4286f7a62f243cb
Tom Sullivan <tom@msbit.com.au>
2019-12-15 16:21:14 +1100

    Use fields to determine initial offset
    Instead of being less robust and hard-coding the initial offset,
    use the `ssl.handshake.cipher_suites_length` field as the basis
    for any further offsets

--- tls-cipher-counter.lua
+++ tls-cipher-counter.lua
@@ -1,16 +1,19 @@
 local ciphers = {}
 
 local function init_listener()
-  local tap = Listener.new("frame", "ssl.handshake.type == 1")
+  local cipher_suites_length = Field.new("ssl.handshake.cipher_suites_length")
+  local tap = Listener.new("ssl", "ssl.handshake.type == 1")
 
   function tap.reset()
     ciphers = {}
   end
 
   function tap.packet(pinfo, tvb, tapinfo)
-    local count = tvb:range(0x8e, 0x2):uint() / 2
-    for i=0, count - 1 do
-      local id = tostring(tvb:range(0x90 + (i * 2), 0x2):bytes())
+    local count_length_offset = -cipher_suites_length()
+    local count = tvb:range(count_length_offset, 0x2):uint() / 2
+    local cipher_index = count_length_offset + 2
+    for i = 0, count - 1 do
+      local id = tostring(tvb:range(cipher_index + (i * 2), 0x2):bytes())
       if type(ciphers[id]) == 'nil' then
         ciphers[id] = 0
       end
